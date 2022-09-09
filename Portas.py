24035e0821be112f9b44ec49a4b1ec298070bc2a
LA <96542543+LaraLdA@users.noreply.github.com>
2022-09-09 17:54:08 -0300

    Delete Portas.py

--- Portas.py
+++
@@ -1,5 +0,0 @@
-import socket
-
-print(socket.getservbyname("domain")) # porta que converte dns e ip
-print(socket.getservbyname("http")) # porta de navegar nas paginas web
-print(socket.getservbyname("ftp")) # usado para transferencia de arquivos
